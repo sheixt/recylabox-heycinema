c2aab634467d80568854abeb33d9b8fd309364e7
George Burrell <sheixt@gmail.com>
2020-11-07 15:46:14 +0000

    Font handling & basic App layout

--- App.tsx
+++ App.tsx
@@ -1,21 +1,45 @@
+import {AppLoading} from 'expo'
+import {useFonts} from 'expo-font'
 import {StatusBar} from 'expo-status-bar'
 import React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
+import {StyleSheet, View, ViewStyle} from 'react-native'
 
-export default function App() {
+import branding from './branding'
+import Header from './components/Header'
+import MovieList from './components/MovieList'
+import SearchForm from './components/SearchForm'
+
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    helveticaNeue: require('./assets/HelveticaNeue.ttf'),
+    helveticaNeueItalic: require('./assets/HelveticaNeueIt.ttf'),
+    helveticaNeueBold: require('./assets/HelveticaNeueBd.ttf'),
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Header />
+      <SearchForm placeholder="Search for a movie title" />
+      <MovieList headline="Movie results:" />
       <StatusBar style="auto" />
     </View>
   )
 }
 
-const styles = StyleSheet.create({
+interface Styles {
+  container: ViewStyle
+}
+
+const styles = StyleSheet.create<Styles>({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: branding.colors.lightGrey,
     alignItems: 'center',
-    justifyContent: 'center',
   },
 })
+
+export default App
